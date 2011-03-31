533fa83a3bef4024fe8d128741e40d074a78e7cf
Jason Felice <jason.m.felice@gmail.com>
2011-03-30 21:41:40 -0400

    Document cucumber steps

--- lib/icuke/icuke_world.rb
+++ lib/icuke/icuke_world.rb
@@ -19,6 +19,29 @@ After do
   icuke_driver.quit
 end
 
+##
+# :section: Loading the iOS Application
+#
+# Launching the application is the most difficult part.  Many parts can be
+# omitted, but the full form looks like this:
+#
+#   Given "iCuke" from "app/iCuke/iCuke.xcodeproj" with build configuration "Debug" is loaded in the retina iphone simulator with SDK 4.0
+#
+# In this case:
+# * "iCuke" is the name of the app to load.
+# * "app/iCuke/iCuke.xcodeproj" is the path to the Xcode project.
+# * "Debug" specifies the build configuration.  "Debug" is used if not
+#   specified.
+# * "retina" or "non-retina" can optionally be specified.  If specified, the
+#   simulator is configured for retina or non-retina version of the device
+#   before launching the application.  If not specified, the simulator
+#   chooses, which means that it will run with the last selected resolution.
+# * "iphone" or "ipad" selects the device type.  "iphone" is the default.
+# * The optional clause "with SDK 4.0" selects how iCuke interacts with
+#   application.  If not specified, iCuke attempts to guess based on which SDKs
+#   are installed; however, it can guess wrong.  Possible values are "3.1" and
+#   "4.0".  "4.0" should be used on 4.2 and newer SDKs
+#
 Given /^(?:"([^\"]*)" from )?"([^\"]*)"(?: with build configuration "([^\"]*)")? is loaded in the (?:(retina|non-retina) )?(?:(iphone|ipad) )?simulator(?: with SDK ([0-9.]+))?$/ do |target, project, configuration, retina, platform, sdk_version|
   if sdk_version
     ICuke::SDK.use(sdk_version)
@@ -43,6 +66,14 @@ Given /^the module "([^\"]*)" is loaded in the simulator$/ do |path|
   simulator.load_module(File.expand_path(path))
 end
 
+##
+# :section: Seeing What's on the Screen
+#
+# The two following rules detect accessibility text on the screen:
+#
+#   Then I should see "some text" within "some scope"
+#   Then I should not see "some text" within "some scope"
+#
 Then /^I should see "([^\"]*)"(?: within "([^\"]*)")?$/ do |text, scope|
   raise %Q{Content "#{text}" not found in: #{screen.xml}} unless icuke_driver.screen.visible?(text, scope)
 end
@@ -51,6 +82,20 @@ Then /^I should not see "([^\"]*)"(?: within "([^\"]*)")?$/ do |text, scope|
   raise %Q{Content "#{text}" was found but was not expected in: #{screen.xml}} if icuke_driver.screen.visible?(text, scope)
 end
 
+##
+# :section: Sending Input to the App
+#
+# The following rules are available:
+#
+#   When I tap "some label"
+#   When I type "some text" in "some field label"
+#   When I drag from <source> to <destination>
+#   When I select the "some label" slider and drag <distance> pixels <direction>
+#   When I move the "label" slider to <n> percent
+#   When I scroll <direction> to "some text"
+#   When I suspend the application
+#   When I resume the application
+#
 When /^I tap "([^\"]*)"$/ do |label|
   icuke_driver.tap(label)
 end
@@ -87,6 +132,16 @@ When /^I resume the application/ do
   icuke_driver.resume
 end
 
+##
+# :section: Debugging Tools
+#
+#   Then I put the phone into recording mode
+#
+#   Then show me the screen
+#
+# "show me the screen" is the quintessential step used for debugging tests.  It dumps
+# the raw XML received from the iOS app.
+#
 Then /^I put the phone into recording mode$/ do
   icuke_driver.record
 end
