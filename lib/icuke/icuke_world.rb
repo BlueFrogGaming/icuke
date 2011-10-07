c72efd1c8ed2ef86facccc97a56cf96364bdb2c8
Jason Felice <jason.m.felice@gmail.com>
2011-10-07 10:03:53 -0400

    Useful errors when using cucumber_compat.rb

--- lib/icuke/icuke_world.rb
+++ lib/icuke/icuke_world.rb
@@ -73,11 +73,11 @@ end
 #   Then I should not see "some text" within "some scope"
 #
 Then /^I should see "([^\"]*)"(?: within "([^\"]*)")?$/ do |text, scope|
-  raise %Q{Content "#{text}" not found in: #{screen.xml}} unless icuke_driver.screen.visible?(text, scope)
+  raise %Q{Content "#{text}" not found in: #{icuke_driver.screen.xml}} unless icuke_driver.screen.visible?(text, scope)
 end
 
 Then /^I should not see "([^\"]*)"(?: within "([^\"]*)")?$/ do |text, scope|
-  raise %Q{Content "#{text}" was found but was not expected in: #{screen.xml}} if icuke_driver.screen.visible?(text, scope)
+  raise %Q{Content "#{text}" was found but was not expected in: #{icuke_driver.screen.xml}} if icuke_driver.screen.visible?(text, scope)
 end
 
 ##
